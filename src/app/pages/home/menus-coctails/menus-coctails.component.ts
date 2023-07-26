2884963df34098b915d0abab25e8ede503138cca
Simon <snsimonescobar@gmail.com>
2023-07-26 03:20:57 -0500

    services added

--- src/app/pages/home/menus-coctails/menus-coctails.component.ts
+++ src/app/pages/home/menus-coctails/menus-coctails.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoctailsService } from 'src/app/services/coctails.service';
 
 @Component({
   selector: 'app-menus-coctails',
   templateUrl: './menus-coctails.component.html',
   styleUrls: ['./menus-coctails.component.scss']
 })
-export class MenusCoctailsComponent {
+export class MenusCoctailsComponent implements OnInit {
+
+  constructor(
+    private coctailService: CoctailsService,
+  ){}
+
+  ngOnInit(): void {
+    this.coctailService.getDrinks().subscribe((coctails: any) => {
+      console.log('coctails list: ', coctails);
+    })
+  }
 
 }
